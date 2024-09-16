a60729ccfd9bee9593cb76f1b42b883c5c07e7b6
meganwu <meganbxwu@gmail.com>
2024-09-16 12:00:53 -0400

    fix link accessibility

--- App/components/ui/Footer.tsx
+++ App/components/ui/Footer.tsx
@@ -5,7 +5,7 @@ import posthog from "posthog-js";
 
 export default function Footer() {
   return (
-    <footer className="container mx-auto my-5 flex h-16 flex-col items-center justify-end space-y-3 border-t px-24 pt-4 text-center sm:h-20 sm:flex-row sm:pt-2 md:text-lg">
+    <footer className="z-50 container mx-auto my-5 flex h-16 flex-col items-center justify-end space-y-3 border-t px-24 pt-4 text-center sm:h-20 sm:flex-row sm:pt-2 md:text-lg">
       {/* <div className="flex items-center space-x-6">
         <div className="flex flex-row space-x-3 pt-2 pb-8 sm:pb-4 md:pt-0 md:pb-0">
           <SocialIcon
@@ -42,9 +42,9 @@ export default function Footer() {
           />
         </div>
       </div> */}
-      <div>
+      <div className="z-50">
         <Link
-          className="text-sm text-foreground/50 hover:text-foreground/80"
+          className="text-sm text-foreground/50 hover:text-foreground/80 z-50"
           href="/privacy-policy"
         >
           Privacy Policy

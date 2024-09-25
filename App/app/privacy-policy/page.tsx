8362b2c5ce491e7506c6b4f56212d9d49f73dcce
meganwu <meganbxwu@gmail.com>
2024-09-25 09:26:54 -0400

    update privacy policy

--- App/app/privacy-policy/page.tsx
+++ App/app/privacy-policy/page.tsx
@@ -7,7 +7,6 @@ const PrivacyPolicy = () => {
       <Header />
       <div className="container mx-auto px-16 py-8 md:px-32 md:py-12">
         <h1 className="text-3xl font-bold mb-4">VoCal Privacy Policy 🔒</h1>
-
         <h2 className="text-2xl font-bold mb-2">Introduction</h2>
         <p className="mb-4">
           Vocal is a voice-based calendar management app that connects to Google
@@ -17,7 +16,6 @@ const PrivacyPolicy = () => {
           ensures compliance with relevant data protection regulations and
           Google’s requirements for apps that access personal information.
         </p>
-
         <h2 className="text-2xl font-bold mb-2">Data We Collect</h2>
         <h3 className="text-xl font-bold mb-2">Personal Data</h3>
         <p className="mb-4">
@@ -39,7 +37,6 @@ const PrivacyPolicy = () => {
           operating system, app version, and unique device identifiers, to
           improve app performance and troubleshoot issues.
         </p>
-
         <h2 className="text-2xl font-bold mb-2">How We Use Your Data</h2>
         <p className="mb-4">
           Voice Processing: Voice inputs are used to process your requests,
@@ -53,13 +50,19 @@ const PrivacyPolicy = () => {
           Improving Services: Non-identifiable data is used to analyze usage
           patterns and improve our services.
         </p>
-
         <h2 className="text-2xl font-bold mb-2">Data Sharing and Storage</h2>
         <h3 className="text-xl font-bold mb-2">Third-Party Services</h3>
         <p className="mb-4">
           Vocal uses Google APIs to connect with your Google Calendar. Your data
           is shared with Google to execute voice commands. We do not share your
           personal information with any other third parties.
+        </p>
+        <h3 className="text-xl font-bold mb-2">Google User Data Privacy</h3>
+        <p className="mb-4">
+          Our app does not transfer or share any Google User data for
+          developing, training, or improving generalized or non-personalized
+          AI/ML models. We are committed to protecting your privacy and ensuring
+          that your data is used responsibly and securely.
         </p>
         <h3 className="text-xl font-bold mb-2">Data Storage</h3>
         <p className="mb-4">
@@ -68,7 +71,6 @@ const PrivacyPolicy = () => {
           stored for processing but are not retained after the interaction is
           completed unless required for service improvement or error resolution.
         </p>
-
         <h2 className="text-2xl font-bold mb-2">User Control Over Data</h2>
         <h3 className="text-xl font-bold mb-2">Data Access</h3>
         <p className="mb-4">
@@ -84,7 +86,6 @@ const PrivacyPolicy = () => {
           . Upon request, we will delete your data, unless it is required for
           legal or security purposes.
         </p>
-
         <h2 className="text-2xl font-bold mb-2">
           Compliance with Google Policies
         </h2>
@@ -94,7 +95,6 @@ const PrivacyPolicy = () => {
           periodic reviews to ensure continued compliance with data protection
           requirements.
         </p>
-
         <h2 className="text-2xl font-bold mb-2">
           Changes to the Privacy Policy
         </h2>
